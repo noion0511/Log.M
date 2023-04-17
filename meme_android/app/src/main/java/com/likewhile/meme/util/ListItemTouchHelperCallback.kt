fbb17a84b5be4ed36ae5e9b439b674a102599704
noion0511 <noion0511@gmail.com>
2023-04-17 20:38:58 +0900

    Revert "fear : 리스트 메모의 마지막 아이템의 순서 변경 제한"
    This reverts commit e6cc396c72ef7a3589f691657a605c1bbbcf5d0b.

--- meme_android/app/src/main/java/com/likewhile/meme/util/ListItemTouchHelperCallback.kt
+++ meme_android/app/src/main/java/com/likewhile/meme/util/ListItemTouchHelperCallback.kt
@@ -14,7 +14,7 @@ class ListItemTouchHelperCallback(private val adapter: ListAdapter) : ItemTouchH
         recyclerView: RecyclerView,
         viewHolder: RecyclerView.ViewHolder
     ): Int {
-        return if (enabled && viewHolder.itemViewType == ListAdapter.ITEM_TYPE_NORMAL) {
+        return if (enabled) {
             val dragFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN
             val swipeFlags = ItemTouchHelper.END
             makeMovementFlags(dragFlags, swipeFlags)
@@ -27,9 +27,6 @@ class ListItemTouchHelperCallback(private val adapter: ListAdapter) : ItemTouchH
         viewHolder: RecyclerView.ViewHolder,
         target: RecyclerView.ViewHolder
     ): Boolean {
-        if (target.itemViewType == ListAdapter.ITEM_TYPE_ADD_BUTTON || viewHolder.itemViewType == ListAdapter.ITEM_TYPE_ADD_BUTTON) {
-            return false
-        }
         adapter.onItemMove(viewHolder.layoutPosition, target.layoutPosition)
         viewHolder.itemView.setBackgroundColor(ContextCompat.getColor(viewHolder.itemView.context, R.color.teal_700))
         return true
